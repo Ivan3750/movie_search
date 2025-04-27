333a61baa2f75c0ad03a3a38813e947f9d2e0bef
Ivan Kohan <kohan3750@gmail.com>
2025-04-27 18:14:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import MovieDetails from "./pages/MovieDetails";
-import Cast from "./components/Cast";
-import Reviews from "./components/Reviews";
-import "./App.css"
+import { lazy, Suspense } from "react"; 
+import "./App.css";
+
+const Layout = lazy(() => import("./components/Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const Movies = lazy(() => import("./pages/Movies"));
+const MovieDetails = lazy(() => import("./pages/MovieDetails"));
+const Cast = lazy(() => import("./components/Cast"));
+const Reviews = lazy(() => import("./components/Reviews"));
+
 function App() {
   return (
     <Router basename="/movie_search">
